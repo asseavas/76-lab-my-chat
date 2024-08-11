24a591e9ec7836e1b5480d938dd73c30841f6fe9
Klimova Alina <alinaklimova82@gmail.com>
2024-08-12 01:40:59 +0600

    I created an API with a route for creating messages and saving it in the database.

---
+++ chat-api/fileDb.ts
@@ -0,0 +1,34 @@
+import {promises as fs} from 'fs';
+import {randomUUID} from 'crypto';
+import {Message, MessageMutation} from './types';
+
+const fileName = './db.json';
+
+let data: Message[] = [];
+
+const fileDb = {
+  async init() {
+    try {
+      const fileContents = await fs.readFile(fileName);
+      data = JSON.parse(fileContents.toString());
+    } catch (e) {
+      data = [];
+    }
+  },
+  async addItem(item: MessageMutation) {
+    const message: Message = {
+      ...item,
+      datetime: new Date().toISOString(),
+      id: randomUUID(),
+    };
+
+    data.push(message);
+    await this.save();
+    return message;
+  },
+  async save() {
+    await fs.writeFile(fileName, JSON.stringify(data, null, 2));
+  }
+};
+
+export default fileDb;
---
+++ chat-api/routes/messages.ts
@@ -0,0 +1,23 @@
+import express from 'express';
+import {MessageMutation} from '../types';
+import fileDb from '../fileDb';
+
+const messagesRouter = express.Router();
+
+messagesRouter.post('/', async (req, res) => {
+  if (!req.body.author || !req.body.message) {
+    return res.status(400).send({'error': 'Author and message must be present in the request'});
+  }
+
+  const message: MessageMutation = {
+    author: req.body.author,
+    message: req.body.message,
+  };
+
+  const savedMessage = await fileDb.addItem(message);
+
+  res.send(savedMessage);
+});
+
+
+export default messagesRouter;
